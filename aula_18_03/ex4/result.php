76b8ef1e0a2de9d2b1fab661a35877115c41839a
Rafael Diehl <rafaeljuliani1984@gmail.com>
2022-03-22 19:35:28 -0300

    fix bugs

--- aula_18_03/ex4/result.php
+++ aula_18_03/ex4/result.php
@@ -16,15 +16,7 @@
 
   include 'includes/header.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
+
   <div class="result">
     <h1>
     <?php
