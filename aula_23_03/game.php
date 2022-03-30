8e65c62013769217c620d114540c727f56f72865
Rafael Juliani Diehl <rafaeljuliani1984@gmail.com>
2022-03-30 19:05:03 -0300

    fix header include bug

--- aula_23_03/game.php
+++ aula_23_03/game.php
@@ -101,15 +101,6 @@
   include 'includes/header.php';
 
 ?>
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Jogo da Velha</title>
-</head>
-<body>
 
   <header>
     <?php if(!$_SESSION["jogoTerminou"]): ?>
